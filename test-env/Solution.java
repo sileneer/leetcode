2002181b1df3465ba99233501e15e3cbb4e90f74
sileneer <31238371+sileneer@users.noreply.github.com.>
2024-06-01 19:25:33 +0800

    Initial commit

--- test-env/Solution.java
+++ test-env/Solution.java
@@ -53,17 +53,6 @@ class Solution {
     }
 
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], i);
-        }
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(target - nums[i])) {
-                if (map.get(target - nums[i]) != i) {
-                    return new int[]{map.get(target - nums[i]), i};
-                }
-            }
-        }
-        return new int[]{-1, -1};
+        return null;
     }
 }
